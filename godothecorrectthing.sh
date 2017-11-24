8a173303ff1f111f8b0cfd50010214007bec0d6e
Andrew Chambers <andrewchambers@fastmail.com>
2017-11-24 22:18:29 +1300

    Fixes a few minor problems, adds a lot of documentation, add a new command.
    The new command is as follows:
    
    Search a given file for a pattern:
     /some/file:/REGEX
    
    Search a dir for a pattern:
     /some/dir/*:/REGEX
    
    Recursively search a dir for a pattern:
     /some/dir/**:/REGEX
    
    Search uses grep -E patterns, binary files are not searched.
    If there are multiple matches, dmenu is used to let the
    user select the correct choice, then open the file with editor.

--- godothecorrectthing.sh
+++ godothecorrectthing.sh
@@ -4,6 +4,13 @@ set -e
 set -u
 set -x
 
+# N.B. This disables globbingg.
+# If you need it, enable it temporarily.
+# This script turns it off because it deals with
+# strings that look like paths, but we don't always
+# want to expand them whenever doing $somevar.
+set -f
+
 editor=subl
 
 guesscwdwithmagic () {
@@ -30,59 +37,133 @@ guesscwdwithmagic () {
 }
 
 cwd=$(guesscwdwithmagic)
+text=$(xclip -o | head -n 1)
 
 manualexpand () {
 	# hack to expand from string without eval
 	case $1 in
 		/*)
-			echo $1
+			readlink -f $1
 		;;
 		~*)
-			echo $HOME/$(echo $1 | cut -c 2-)
+			readlink -f $HOME/$(echo $1 | cut -c 2-)
 		;;
 		*)
-			echo $cwd/$1
+			readlink -f $cwd/$1
 		;;
 	esac	
 }
 
-cwd=$(manualexpand $cwd)
-text=$(xclip -o | head -n 1)
+# Match URL's
 
-case $text in
+case "$text" in
 	http://* | https://*)
 		exec xdg-open $text
 	;;
 esac
 
-if echo $text  | grep -q -E '^[a-zA-Z/~ \.]+(:[0-9]*)*:?'
+
+# Match patterns of the form:
+#
+# /some/file.txt:line:col
+#
+# and then open the text editor.
+
+if echo "$text" | grep -q -E '^[_a-zA-Z0-9/~ \.]+:[0-9]+(:[0-9]+)?:?'
 then
-	fwithpos=$(manualexpand $text)
+	f=$(manualexpand $(echo $text | cut -d : -f 1))
+	pos=$(echo $text | cut -d : -f 2-)
+	fwithpos=$f:$pos
 
 	# strip trailing :, go error messages are one place this happens
-	case $(echo $fwithpos | rev) in
-		:*)
+	case $fwithpos in
+		*:)
 			fwithpos=$(echo $fwithpos | rev | cut -c 2- | rev)
 		;;
 	esac
 
-	fnopos=$fwithpos
-	if echo $fwithpos | grep -q -E ':'
-	then
-		fnopos=`echo $fnopos | cut -d : -f 1`
-	fi
-
+	fnopos=`echo $fwithpos | cut -d : -f 1`
+	
 	if test -f $fnopos
 	then
-		case $fnopos in
-			*)
-				exec $editor $fwithpos
+		exec $editor $fwithpos
+	fi
+fi
+
+# Search a given file for a pattern:
+# /some/file:/REGEX
+#
+# Search a dir for a pattern:
+# /some/dir/*:/REGEX
+#
+# Recursively search a dir for a pattern:
+#  /some/dir/**:/REGEX
+#
+# Search uses grep -E patterns, binary files are not searched.
+# If there are multiple matches, dmenu is used to let the
+# user select the correct choice, then open the file with editor.
+#
+# Search does not require dmenu to function when there is only a single match,
+# but a much better experience requires dmenu.
+
+if echo "$text" | grep -q -E '^[_a-zA-Z0-9/~ \.]+\*?\*?:/.+'
+then
+	f=$(echo $text | cut -d : -f 1)
+	maxdepth="-maxdepth 0"
+	case "$f" in
+		*\*\*)
+			maxdepth=""
+			f=$(echo $f | rev | cut -c 3- | rev)
+		;;
+		*\*)
+			maxdepth="-maxdepth 1"
+			f=$(echo $f | rev | cut -c 2- | rev)
+		;;
+	esac
+	f=$(manualexpand $f)
+	pat=$(echo $text | cut -d : -f 2- | cut -c 2-)
+
+	if test -e $f
+	then
+		# use find because some grep implementations don't have --exclude-dirs.
+		filestosearch=$(find $f $maxdepth -not -path '*/\.*' -type f)
+		searchresultsf=`mktemp`
+		match=""
+		grep -I -H -n -E "$pat" $filestosearch > $searchresultsf
+		wait
+		nresults=$(wc -l $searchresultsf | cut -d ' ' -f 1)
+		if test $nresults -gt 1
+		then
+			echo foobar
+			match=$(cat $searchresultsf | dmenu -l 10 | cut -d : -f 1-2)
+		elif test $nresults -gt 0
+		then
+			match=$(cat $searchresultsf | cut -d : -f 1-2)
+		fi
+		rm $searchresultsf
+		if test -n "$match"
+		then
+			exec $editor $match
+		fi
+	fi
+fi
+
+# Match plain unadorned files or directories.
+
+if echo "$text" | grep -q -E '^[_a-zA-Z0-9/~ \.]+'
+then
+	f=$(manualexpand $text)
+	if test -e $f
+	then
+		case $f in
+			# because xdg-open cannot handle line numbers
+			# anyway, we must have the $editor hack.
+			# We should use it for text files for consistency.
+			*.c | *.go | *.md | *.txt)
+				exec $editor $f	
 			;;
 		esac
-	fi
 
-	if test -d $fnopos
-	then
-		exec xdg-open $fnopos
+		exec xdg-open $f
 	fi
 fi
